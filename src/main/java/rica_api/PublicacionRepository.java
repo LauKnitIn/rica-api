eb3e2f1ce53f1615e82dfb0a2b95e623c91ded70
Laura Correa <correabsofial@gmail.com>
2026-09-09 14:55:47 -0500

    add: interfaz

---
+++ src/main/java/rica_api/PublicacionRepository.java
@@ -0,0 +1,10 @@
+package rica_api;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PublicacionRepository extends MongoRepository<Publicacion, String>{
+     
+    List<Publicacion> findByInvestigadorCorreo(String investigadorCorreo);
+}
